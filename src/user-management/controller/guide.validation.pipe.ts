d862a039489bb42e6fc8ef0f13a9755fab2ebfca
yobahBertrandYonkou <62288787+yobahBertrandYonkou@users.noreply.github.com>
2024-12-22 00:06:14 +0100

    feat: add guide controller and validation pipe

---
+++ src/user-management/controller/guide.validation.pipe.ts
@@ -0,0 +1,54 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+import { log } from 'console';
+import { GuideVO } from '../vo/user.guide.vo';
+import { UpdateGuideDTO } from './dto/guide.update.dto';
+import { CreateGuideDTO } from './dto/guide.create.dto';
+
+@Injectable()
+export class GuideValidationPipe
+  implements PipeTransform<any, Promise<GuideVO>>
+{
+  constructor(private readonly origin: string = 'default') {}
+  async transform(value: any, metadata: ArgumentMetadata): Promise<GuideVO> {
+    log(metadata);
+    // checking if value if empty
+    if (!value) {
+      throw new BadRequestException('Request body cannot be empty');
+    }
+
+    // validate input data against GuideDTO
+    const guideDto =
+      this.origin == 'update'
+        ? plainToInstance(UpdateGuideDTO, value)
+        : plainToInstance(CreateGuideDTO, value);
+    const errors = await validate(guideDto);
+
+    // checking if there are any errors
+    if (errors.length > 0) {
+      throw new BadRequestException(errors);
+    }
+
+    // transform data into GuidVO
+    return plainToInstance(GuideVO, value);
+  }
+}
+
+@Injectable()
+export class HasAttribute implements PipeTransform<any, string | number> {
+  constructor(private readonly parameters: string[]) {}
+  transform(value: any, metadata: ArgumentMetadata): string | number {
+    if (!value || !this.parameters.every((param) => param in value)) {
+      throw new BadRequestException(
+        `Body must contain { ${this.parameters.join(', ')} }`,
+      );
+    }
+    return value;
+  }
+}

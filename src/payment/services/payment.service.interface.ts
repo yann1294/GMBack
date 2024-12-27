2b0e11df9acfd9388a846ee955078a99d7689768
yobahBertrandYonkou <62288787+yobahBertrandYonkou@users.noreply.github.com>
2024-12-27 18:31:54 +0100

    refactor: comment unrequired models

--- src/payment/services/payment.service.interface.ts
+++ src/payment/services/payment.service.interface.ts
@@ -3,9 +3,9 @@ import { PaymentVO } from "../vo/payment.master.vo";
 
 export interface IPaymentService {
     processPayment(payment: PaymentVO): Promise<ResponseObject>;
-    getTransactionHistory(): Promise<ResponseObject>;
-    confirmPayment(): Promise<ResponseObject>;
-    generatePaymentReceipt(): Promise<ResponseObject>;
-    // getBookingFees(): Promise<ResponseObject>;
     savePaymentDetails(): Promise<ResponseObject>;
+    // getTransactionHistory(): Promise<ResponseObject>;
+    // confirmPayment(): Promise<ResponseObject>;
+    // generatePaymentReceipt(): Promise<ResponseObject>;
+    // getBookingFees(): Promise<ResponseObject>;
 }

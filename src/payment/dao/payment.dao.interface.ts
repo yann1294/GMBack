ad2b1d4a4e033e53027f0d82d71b448f8a9d54a2
yobahBertrandYonkou <62288787+yobahBertrandYonkou@users.noreply.github.com>
2024-12-31 19:37:13 +0100

    refactor(payment): changed all servcie params to vo

--- src/payment/dao/payment.dao.interface.ts
+++ src/payment/dao/payment.dao.interface.ts
@@ -1,10 +1,11 @@
 import { ResponseObject } from 'src/shared/types';
 import { Payment } from './payment.entity';
+import { Tourist } from 'src/booking/vo/helper.vo';
 
 export interface IPaymentDAO {
   create(payment: Payment): Promise<ResponseObject>;
-  update(id: string, payment: Payment): Promise<ResponseObject>;
-  findById(id: string): Promise<ResponseObject>;
-  delete(id: string): Promise<ResponseObject>;
-  findAll(userId: string): Promise<ResponseObject>;
+  update(payment: Payment): Promise<ResponseObject>;
+  findById(payment: Payment): Promise<ResponseObject>;
+  delete(payment: Payment): Promise<ResponseObject>;
+  findAll(tourist: Tourist): Promise<ResponseObject>;
 }

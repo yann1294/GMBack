a148aa9aba2317ebb110c4c7685df3304f6640eb
yobahBertrandYonkou <62288787+yobahBertrandYonkou@users.noreply.github.com>
2025-01-27 17:56:58 +0100

    fix: changed BookingStatus to status

--- src/booking/vo/booking.master.vo.ts
+++ src/booking/vo/booking.master.vo.ts
@@ -2,14 +2,14 @@ import { Booking } from '../dao/booking.entity';
 import { Expose, instanceToPlain, Type } from 'class-transformer';
 import { IsIn, IsOptional, ValidateNested } from 'class-validator';
 import { Tourist } from './helper.vo';
-import { BookingStatus } from '../utils/consts.utils';
+import { BookingStatus, status } from '../utils/consts.utils';
 
 export class BookingVO {
   @Expose({ name: 'id' })
   @IsOptional()
   private _id: string;
   @Expose({ name: 'status' })
-  @IsIn(BookingStatus)
+  @IsIn(status)
   @IsOptional()
   private _status: string;
   @Expose({ name: 'bookedOn' })
